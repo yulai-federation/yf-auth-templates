1dd107f2d19d16b0eb464ba09650f34eb977072b
Peter Pfeufer <info@ppfeufer.de>
2021-05-07 22:37:21 +0200

    pre-commit updated and applied

--- yf_templates/apps.py
+++ yf_templates/apps.py
@@ -6,4 +6,4 @@ from . import __version__
 class YfTemplatesConfig(AppConfig):
     name = "yf_templates"
     label = "yf_templates"
-    verbose_name = "Yulai Federation Auth Templates v{}".format(__version__)
+    verbose_name = f"Yulai Federation Auth Templates v{__version__}"
